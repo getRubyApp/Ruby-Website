9de2d437ddb883c75ad8b7ccdb5bcacdb22136fb
Nick Oates <nick@nickoates.com>
2024-06-29 11:55:17 -0700

    Aurora

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { AppStoreIcon } from "@/components/Icons";
 import Hero from "@/assets/Hero.webp";
 import Image from "next/image";
+import { Aurora } from "@/components/Aurora";
 
 export default function Home() {
 	return (
 		<main className="lg:py-20 py-8 sm:py-12">
+			<Aurora />
+
 			<p className="font-black text-4xl lg:text-5xl text-center drop-shadow tracking-tight text-transparent bg-clip-text bg-gradient-to-br from-pink-600 to-pink-800 dark:from-pink-200 dark:to-pink-400">
 				News that Shines
 			</p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,39 @@
 import type { Config } from "tailwindcss";
 import typography from "@tailwindcss/typography";
+// @ts-ignore This is a hack to get the tailwind colors
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 
 const config: Config = {
 	content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
 		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			animation: {
+				aurora: "aurora 60s linear infinite",
+			},
+			keyframes: {
+				aurora: {
+					from: {
+						backgroundPosition: "50% 50%, 50% 50%",
+					},
+					to: {
+						backgroundPosition: "350% 50%, 350% 50%",
+					},
+				},
 			},
 		},
 	},
-	plugins: [typography],
+	plugins: [typography, addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+	const allColors = flattenColorPalette(theme("colors"));
+	const newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	);
+
+	addBase({
+		":root": newVars,
+	});
+}
 
 export default config;
